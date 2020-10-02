c87e4dc579ce33aa599adaa3fa1db9febd907476
rtheni <rntm72@gmail.com>
2020-10-02 16:05:09 -0300

    port updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const userRoutes = require("./routes/userRoutes");
 const songRoutes = require("./routes/songRoutes");
